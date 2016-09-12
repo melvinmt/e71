4c11a68931219751cddae798203be4fbce0c9b2b
Melvin Tercan <mmtercan@gmail.com>
2016-09-12 12:30:04 -0700

    initial commit

---
+++ HelloWorld/src/com/melvin/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.melvin;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!!");
+        }
+
+    }
+}
